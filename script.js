552db900f6dfeca73c9ce8a11ae8b7b6399761f1
srinivasu619 <srinivasu.618@gmail.com>
2017-07-31 00:55:32 +0530

    Added Funtionality to the tweet button

--- script.js
+++ script.js
@@ -17,8 +17,10 @@ function getQuote(num)
 {
     $.get("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40&callback=",function(data)
                     {
-      $("#quote-title").text(data[num].title);
+      $("#quote-title").text("By "+data[num].title);
       $("#quote").html(data[num].content);
+      var url = "https://twitter.com/intent/tweet?text="+data[num].content.slice(3,-5)+" -By "+data[num].title;
+      $("#tweetQuote").attr("href",url);
     });
   //console.log(data);
 }
